80f5bebe0d4910d0f1f23528499b6a87b9671687
DorinSC <scofertadorinsergiu@gmail.com>
2022-04-11 12:55:54 +0300

    finish lab5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+
+import { Layout } from 'antd'
+import { useRootStore } from './index'
+import { HeaderLayout } from './Layouts/HeaderLayout'
+import { ContentLayout } from './Layouts/ContentLayout'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const rootStore = useRootStore()
+
+    useEffect(() => {
+        rootStore.setInitialStorageContents()
+    }, [])
+
+    return (
+        <>
+            <Layout className='layout'>
+                <HeaderLayout />
+                <ContentLayout />
+            </Layout>
+        </>
+    )
 }
 
-export default App;
+export default App
